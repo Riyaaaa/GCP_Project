f6ad2ac4f9262b25edf2fea057f24e88b10f0a6c
Riya Srivastava <srivastava.ri@northeastern.edu>
2021-09-01 17:28:02 -0400

    Update DAG.py

--- DAG.py
+++ DAG.py
@@ -29,29 +29,23 @@ dag = DAG(
 )
 
 
-def combine_dataframes():
+def merge_chunks():
     storage_client = storage.Client()
     bucket_name = "consumed-twitter-data1"
     blobs = storage_client.list_blobs(bucket_name)
-    combined_df = pd.DataFrame()
+    merged = pd.DataFrame()
     for blob in blobs:
         file_name = blob.name
         print(file_name)
         blob.download_to_filename(f'/home/airflow/gcs/data/{file_name}')
-        print('Reading Dataframes and combining them')
-        df = pd.read_csv(f'/home/airflow/gcs/data/{file_name}')
-        combined_df = combined_df.append(df)
-        #blob.delete()
-    combined_df.to_csv('/home/airflow/gcs/data/final_df.csv', index = False)
-'''
-def clean_dataframe():
-    df = pd.read_csv('/home/airflow/gcs/data/final_df.csv')
-    for idx, row in df.iterrows():
-        row['text'] = str(row['text'])
-    df.to_csv('/home/airflow/gcs/data/final_df.csv', index = False)
-'''
+        print('Combing data chunks')
+        chunks = pd.read_csv(f'/home/airflow/gcs/data/{file_name}')
+        merged = merged.append(chunks)
+        
+    merged.to_csv('/home/airflow/gcs/data/merged.csv', index = False)
 
-def upload_to_bigquery():
+
+def bucket_to_bq():
     client = bigquery.Client()
     table_id = "tensile-pier-322516.tweetsdata.tweet_info"
     job_config = bigquery.LoadJobConfig(
@@ -65,7 +59,7 @@ def upload_to_bigquery():
         source_format = bigquery.SourceFormat.CSV,
         allow_quoted_newlines = True
     )
-    uri = "gs://us-central1-newcomposer-ff97357e-bucket/data/final_df.csv"
+    uri = "gs://us-central1-newcomposer-ff97357e-bucket/data/merged.csv"
     load_job = client.load_table_from_uri(
         uri, table_id, job_config = job_config
     )
@@ -75,25 +69,21 @@ def upload_to_bigquery():
 
 
 start = DummyOperator(task_id = "start", dag = dag)
+
+task_1_merge_chunks = PythonOperator(
+    task_id = "merge_chunks_1",
+    python_callable = merge_chunks,
+    dag = dag
+)
+task_2_bucket_to_bq = PythonOperator(
+    task_id = "bucket_to_bq_2",
+    python_callable = bucket_to_bq,
+    dag = dag
+)
+
 end = DummyOperator(task_id = "end", dag = dag)
-combine_dataframes_task = PythonOperator(
-    task_id = "combine_dataframes_task",
-    python_callable = combine_dataframes,
-    dag = dag
-)
-upload_to_bigquery_task = PythonOperator(
-    task_id = "upload_to_bigquery_task",
-    python_callable = upload_to_bigquery,
-    dag = dag
-)
-'''
-clean_dataframe_task = PythonOperator(
-    task_id = "clean_dataframe_task",
-    python_callable = clean_dataframe,
-    dag = dag
-)
-'''
-start >> combine_dataframes_task >> upload_to_bigquery_task >> end
+
+start >> task_1_merge_chunks >> task_2_bucket_to_bq >> end
 
 
 
