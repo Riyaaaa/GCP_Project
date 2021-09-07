df975ac98a28b164472486d6ca1a88d434ab7192
Riya Srivastava <srivastava.ri@northeastern.edu>
2021-09-07 15:24:48 -0400

    Update cloudFunction.py

--- cloudFunction.py
+++ cloudFunction.py
@@ -11,10 +11,10 @@ def hello_pubsub(event, context):
     """
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     json_data = json.loads(pubsub_message)
-    #final_jsob_data = {"id":json_data['id'], "text":json_data['text']}
+    
     df = pd.json_normalize(json_data)
     storage_client = storage.Client()
-    #df = self.convert_message_to_dataframe()
+   
     bucket = storage_client.get_bucket('twitter-data-1059')
     blob = bucket.blob('twitter_data{}.csv'.format(context.timestamp))
     blob.upload_from_string(data = df.to_csv(index = False), content_type = 'text/csv')
